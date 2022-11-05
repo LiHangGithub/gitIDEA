45c984c6ce31c85adb519675e2c34291c95666db
lihang <1731454191@qq.com>
2022-11-05 18:54:56 +0800

    Second Commit

--- src/main/java/com/slxy/GitTest.java
+++ src/main/java/com/slxy/GitTest.java
@@ -3,5 +3,6 @@ package com.slxy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello First Git");
+        System.out.println("Hello Second Git");
     }
 }
