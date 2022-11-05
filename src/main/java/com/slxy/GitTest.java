df0375b8b2133ded08184da572aa3da4cf90f0da
lihang <1731454191@qq.com>
2022-11-05 19:15:44 +0800

    HotFix Commit2

--- src/main/java/com/slxy/GitTest.java
+++ src/main/java/com/slxy/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello Three Git");
         System.out.println("Hello Four Git");
         System.out.println("Hello Hot-fix Git");
+        System.out.println("Hello Hot-fix22 Git");
     }
 }
