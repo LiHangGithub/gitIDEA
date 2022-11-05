461deb86f97fc6742c9d1f7138a39ab1d332b7b9
lihang <1731454191@qq.com>
2022-11-05 18:55:22 +0800

    Three Commit

--- src/main/java/com/slxy/GitTest.java
+++ src/main/java/com/slxy/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello First Git");
         System.out.println("Hello Second Git");
+        System.out.println("Hello Three Git");
     }
 }
